66b58c2b71988b04e8b2b1a326a33e9b1bf8712c
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-04-05 09:30:39 -0300

    Aula 5 - Lifecycle/ViewBinding

--- app/src/main/java/com/github/cesar1287/turma3sib/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/turma3sib/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.cesar1287.turma3sib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.i("lifecycle", "onCreate - Main")
+
         val textView = findViewById<TextView>(R.id.tvMainHelloWorld)
         val randomButton = findViewById<Button>(R.id.btMainRandom)
 
@@ -19,5 +22,30 @@ class MainActivity : AppCompatActivity() {
             val text = "O número sorteado é $randomNumber"
             textView.text = text
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("lifecycle", "onStart - Main")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("lifecycle", "onResume - Main")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("lifecycle", "onPause - Main")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("lifecycle", "onStop - Main")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("lifecycle", "onDestroy - Main")
     }
 }
--- app/src/main/java/com/github/cesar1287/turma3sib/RegisterActivity.kt
+++ app/src/main/java/com/github/cesar1287/turma3sib/RegisterActivity.kt
@@ -1,12 +1,65 @@
 package com.github.cesar1287.turma3sib
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.github.cesar1287.turma3sib.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i("lifecycle", "onCreate - Register")
+
+        with(binding) {
+            btRegisterSave.setOnClickListener {
+                val name = tieRegisterName.text.toString()
+                val email = tieRegisterEmail.text.toString()
+                val password = tieRegisterPassword.text.toString()
+
+                if (name.isBlank()) {
+                    tilRegisterName.error = "Campo nome vazio ou inválido"
+                } else {
+                    tilRegisterName.isErrorEnabled = false
+                }
+
+                startActivity(
+                    Intent(
+                        this@RegisterActivity,
+                        MainActivity::class.java)
+                )
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("lifecycle", "onStart - Register")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("lifecycle", "onResume - Register")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("lifecycle", "onPause - Register")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("lifecycle", "onStop - Register")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("lifecycle", "onDestroy - Register")
     }
 }
